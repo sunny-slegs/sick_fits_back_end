323c91bf14d1fe533b05b596658d993b94e5bddf
Sonya <sonjay08@gmail.com>
2018-12-20 08:03:20 -0600

    added db.js file

---
+++ src/db.js
@@ -0,0 +1,13 @@
+//This file connects to the remote prisma DV 
+//and gives us the ability to query with js
+
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false,
+});
+
+module.exports = db;
